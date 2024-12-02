8048b6d035b0aa0b810a83dd5f9998df03257b1b
Nina Zrno <ninaradakovic1234@icloud.com>
2024-12-02 12:57:57 +0100

    Create vjezbazatablicu2.sql

---
+++ vjezbazatablicu2.sql
@@ -0,0 +1,26 @@
+﻿use master;
+go
+drop database if exists vjezbazatablicu2;
+go
+create database vjezbazatablicu2;
+go
+
+use vjezbazatablicu2;
+
+create table vjezbe(
+sifra int,
+trajanje varchar(20),
+vrsta varchar(20),
+izvodiseod datetime
+);
+
+create table tablice21(
+sifra int,
+vrsta varchar(20),
+kolumne int
+);
+
+create table grupiranje(
+vjezba int,
+tablica21 int
+);
